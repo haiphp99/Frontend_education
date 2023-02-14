f9fd46e280f01f80ab89738623cb9d3294c5fd99
haiphp99 <vothanhhai060599@gmail.com>
2023-02-14 08:52:22 +0700

    fe-education

---
+++ assets/js/carousel.js
@@ -0,0 +1,6 @@
+// Carousel Auto-Cycle
+$(document).ready(function() {
+    $('.testimonial__carousel-slide-review').carousel({
+      interval: 6000
+    })
+  });
---
+++ assets/js/menu-reponsive.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.icon').click(function(){
+        
+        // $('.header__menu-list').css('display', 'block');
+        $('.header__menu-list').toggle();
+        console.log('123');
+    })
+})
